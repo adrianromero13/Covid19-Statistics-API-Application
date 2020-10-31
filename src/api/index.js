11c652c3e2087da11749fad4852e682f58ca1d53
Adrian Romero <radrian031613@gmail.com>
2020-10-30 20:57:49 -0700

    using chart.js to render data in form of chart

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,4 @@
+import { Dialog } from '@material-ui/core';
 import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
@@ -12,6 +13,23 @@ export const fetchData = async () => {
       deaths,
       lastUpdate,
     }
+  } catch (e) {
+    
+  }
+}
+
+// daily data
+export const fetchDailyData = async() => {
+  try {
+    const {data } = await axios.get(`${url}/daily`)
+
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+
+    return modifiedData;
   } catch (e) {
     
   }
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
+
+// styling
+import styles from './Chart.module.css';
 
 const Chart = () => {
-    return ( 
-        <h1>Chart</h1>
-    )
+  // state variables
+  const [dailyData, setDailyData] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    }
+
+    fetchAPI();
+  });
+
+  const lineChart = (
+    dailyData.length
+      ? (
+        <Line
+          data={{
+            labels: dailyData.map(({ date }) => date),
+            datasets: [{
+              data: dailyData.map(({ confirmed }) => confirmed),
+              label: 'Infected',
+              borderColor: '#3333ff',
+              fill: true,
+            }, {
+              data: dailyData.map(({ deaths }) => deaths),
+              label: 'Deaths',
+              borderColor: 'red',
+              backgroundColor: 'rgba(255, 0, 0, 0.5)',
+              fill: true,
+            }],
+          }}
+        />
+      ) : null
+  );
+  return (
+    <div className={styles.container}>
+       {lineChart}
+    </div>
+  )
 }
 
 export default Chart;
