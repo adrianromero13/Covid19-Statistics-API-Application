e91c3d1169cec170ea68503d8e8f23e0a9082cf0
Adrian Romero <radrian031613@gmail.com>
2020-11-01 16:10:13 -0800

    set up axios.get function for countries

--- src/api/index.js
+++ src/api/index.js
@@ -5,8 +5,8 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
   try {
-    const { data: { confirmed, recovered, deaths, lastUpdate} } = await axios.get(url);
-    
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+
     return {
       confirmed,
       recovered,
@@ -14,14 +14,14 @@ export const fetchData = async () => {
       lastUpdate,
     }
   } catch (e) {
-    
+
   }
 }
 
 // daily data
-export const fetchDailyData = async() => {
+export const fetchDailyData = async () => {
   try {
-    const {data } = await axios.get(`${url}/daily`)
+    const { data } = await axios.get(`${url}/daily`)
 
     const modifiedData = data.map((dailyData) => ({
       confirmed: dailyData.confirmed.total,
@@ -31,6 +31,16 @@ export const fetchDailyData = async() => {
 
     return modifiedData;
   } catch (e) {
-    
+    return e;
   }
-}
+};
+
+export const fetchCountries = async () => {
+  try {
+    const { data: { countries } } = await axios.get(`${url}/countries`);
+
+   return countries.map((country) => country.name);
+  } catch (e) {
+    return e;
+  }
+};
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+
+import { fetchCountries } from '../../api';
+// import styling
+import styles from './CountryPicker.module.css';
+
 
 const CountryPicker = () => {
-    return ( 
-        <h1>CountryPicker</h1>
+const { fetchedCountries, setFectchedCountries } = useState([]);
+
+    // useEffect
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setFectchedCountries( await fetchCountries);
+        }
+
+        fetchAPI();
+    }, [setFectchedCountries]);
+
+    console.log(fetchedCountries);
+
+    return (
+        <FormControl className={styles.formControl}>
+            <NativeSelect>
+                <option value='global'>Global</option>
+            </NativeSelect>
+        </FormControl>
     )
 }
 
